d6352045ddca7009636f8f460bff1ca41c961c9f
KunjanMinama <231558936+KunjanMinama@users.noreply.github.com>
2026-03-27 22:44:02 +0530

    Fix logger setup: add handler and correct Formatter capitalization

--- server/logger.py
+++ server/logger.py
@@ -8,5 +8,17 @@ def setup_logger(name="MedicalAssistant"):
     ch.setLevel(logging.DEBUG)
 
 
-    formatter=logging.formatter("[%(asctime)s] [%(levelname)s] --- [%(message)s]")
+    formatter=logging.Formatter("[%(asctime)s] [%(levelname)s] --- [%(message)s]")
     ch.setFormatter(formatter)
+
+    if not logger.hasHandlers():
+        logger.addHandler(ch)
+
+    return logger
+
+logger = setup_logger()
+
+logger.info("RAG process started")
+logger.debug("Debugging")
+logger.error("Failed to load")
+logger.critical("Critical message")
