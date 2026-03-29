34e0e0f55f3dca7b686210f568332661e6889eb6
KunjanMinama <231558936+KunjanMinama@users.noreply.github.com>
2026-03-29 13:31:07 +0530

    Created frontend for chat

---
+++ components/chatUI.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import requests
+from utils.api import ask_question
+from datetime import datetime
+
+
+def render_chat():
+    st.subheader(" 🗨️ chat with your assistant")
+
+    if "messages" not in st.session_state:
+        st.session_state.messages=[]
+
+    # render excisting chat history
+    for msg in st.session_state.messages:
+        st.chat_message(msg["role"]).markdown(msg["content"])
+
+    #inout and response 
+    user_input = st.chat_input("Type your question...")
+    if user_input:
+        st.chat_message("user").markdown(user_input)
+        st.session_state.messages.append({"role":"user","content":user_input})
+
+        response=ask_question(user_input)
+        if response.status_code==200:
+            data=response.json()
+            answer=data["response"]
+            sources=data.get("sources",[] )
+            st.chat_message("assistant").markdown(answer)
+           # if sources:
+            #    st.markdown(" 📃 **Sources: **")
+             #   for src in sources:
+              #      st.markdown(f"- '{src}")
+            st.session_state.messages.append({"role":"assistant","content":answer})
+        else:
+            st.error(f"Error: {response.text}")
+                    
+    
