5420f5f2ac78501a5d7cd6737c9f4286f9356d4c
KunjanMinama <231558936+KunjanMinama@users.noreply.github.com>
2026-03-29 13:41:59 +0530

    Added frontend for chat

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from components.chatUI import render_chat
+from components.upload import render_uploader
+from components.history_downloader import render_history_download
+
+st.set_page_config(page_title="AI Medical Assistant",layout="wide")
+st.title(" 👨‍⚕️ Medical Assistant Bot")
+
+render_uploader()
+render_chat()
+render_history_download() 
