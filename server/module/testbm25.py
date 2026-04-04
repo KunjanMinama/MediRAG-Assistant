5b13ea9dff541e263afb199bb8c851743be2efa8
KunjanMinama <231558936+KunjanMinama@users.noreply.github.com>
2026-04-04 13:11:23 +0530

    Updated API_BASE for deployment

--- server/module/testbm25.py
+++
@@ -1,8 +0,0 @@
-from bm25s import BM25
-import inspect
-
-print("BM25 Loaded From:")
-print(inspect.getfile(BM25))
-
-print("\nBM25 Methods:")
-print([m for m in dir(BM25) if not m.startswith("_")])
